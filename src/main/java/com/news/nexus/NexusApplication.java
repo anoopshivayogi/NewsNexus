5f394cd500c439cc304dca30943c04fe69058743
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2023-05-14 21:03:31 -0700

    Added spring init code with mongodb configuration

---
+++ src/main/java/com/news/nexus/NexusApplication.java
@@ -0,0 +1,14 @@
+package com.news.nexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusApplication.class, args);
+	}
+
+
+}
