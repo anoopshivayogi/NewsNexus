f963d815f6d42949edb0ffbe50a0df3a1beb43d2
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2023-05-17 19:11:03 -0700

    Added reviewsform with problem to be resolved

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,10 +6,13 @@ import {Routes, Route} from 'react-router-dom';
 import Home from './components/home/Home'
 import { Header } from './components/header/Header';
 import Trailer from './components/trailer/Trailer';
+import Reviews from './components/reviews/Reviews';
 
 function App() {
 
   const [movies, setMovies] = useState([]);
+  const [movie, setMovie] = useState([]);
+  const [reviews, setReviews] = useState([]);
 
   const getMovies = async () => {
 
@@ -20,6 +23,18 @@ function App() {
       setMovies(response.data);
 
     }catch(err){
+      console.log(err);
+    }
+  }
+
+  const getMovieData = async (movieId) => {
+    try{
+      const respone = await api.get(`/api/v1/movies/imdb/${movieId}`)
+      const singleMovie = respone.data;
+      setMovie(singleMovie.data);
+      setReviews(singleMovie.reviews);
+    }
+    catch(error){
       console.log(err);
     }
   }
@@ -35,6 +50,7 @@ function App() {
         <Route path="/" element={<Layout/>}>
           <Route path="/" element={<Home movies={movies}/>}></Route>
           <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+          <Route path="/Reviews/:movieId" element={<Reviews getMovieData={getMovieData} movie={movie} reviews={reviews} setReviews={setReviews} />}></Route>
         </Route>
       </Routes>
     </div>
