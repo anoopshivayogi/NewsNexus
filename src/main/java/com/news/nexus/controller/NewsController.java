a8b6b351acec7f7d359e6746cd6c63a9ed8651cb
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2023-05-18 17:29:29 -0700

    Added getAllNews GET method to obtain all news articles

---
+++ src/main/java/com/news/nexus/controller/NewsController.java
@@ -0,0 +1,26 @@
+package com.news.nexus.controller;
+
+import com.news.nexus.model.News;
+import com.news.nexus.service.NewsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/news")
+public class NewsController {
+    @Autowired
+    private NewsService newsService;
+
+    @GetMapping
+    public ResponseEntity<List<News>> getAllNews(){
+        return new ResponseEntity<>(newsService.allNews(), HttpStatus.OK);
+    }
+
+
+}
