e7680387de2607b720d0aa1ce8f49db6ed226a8e
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2023-05-19 13:27:17 -0700

    basic layout for dashboard page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Nav from './components/Nav';
+import { ColorModeScript } from '@chakra-ui/react'
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+      <div className="App">
+        <ColorModeScript initialColorMode={"dark"} />
+        <Nav/>
+      </div>
   );
 }
 
