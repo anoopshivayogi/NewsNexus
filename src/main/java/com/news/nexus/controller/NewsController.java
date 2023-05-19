7770a74dfed0c279ca36d4db4ebe544859c73d9d
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2023-05-18 20:01:50 -0700

    Basic REST APIs are implemented

--- src/main/java/com/news/nexus/controller/NewsController.java
+++ src/main/java/com/news/nexus/controller/NewsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,16 @@ public class NewsController {
     @GetMapping
     public ResponseEntity<List<News>> getAllNews(){
         return new ResponseEntity<>(newsService.allNews(), HttpStatus.OK);
+    }
+
+    @GetMapping("/source/{sourceId}")
+    public ResponseEntity<List<News>> newsBySource_id(@PathVariable String sourceId){
+        return new ResponseEntity<>(newsService.newsBySource_id(sourceId), HttpStatus.OK);
+    }
+
+    @GetMapping("/category/{category}")
+    public ResponseEntity<List<News>> getNewsByCategoryOrderByPubDate(@PathVariable String category){
+        return new ResponseEntity<>(newsService.newsByCategoryOrderByPubDate(category), HttpStatus.OK);
     }
 
 
