b262595c81f482fd3c1664667b3fe265d6d83c91
Restyled.io <commits@restyled.io>
2023-05-19 17:41:08 +0000

    Restyled by clang-format

--- src/main/java/com/news/nexus/controller/NewsController.java
+++ src/main/java/com/news/nexus/controller/NewsController.java
@@ -2,6 +2,7 @@ package com.news.nexus.controller;
 
 import com.news.nexus.model.News;
 import com.news.nexus.service.NewsService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,28 +11,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api/v1/news")
 public class NewsController {
-    @Autowired
-    private NewsService newsService;
+  @Autowired private NewsService newsService;
 
-    @GetMapping
-    public ResponseEntity<List<News>> getAllNews(){
-        return new ResponseEntity<>(newsService.allNews(), HttpStatus.OK);
-    }
+  @GetMapping
+  public ResponseEntity<List<News>> getAllNews() {
+    return new ResponseEntity<>(newsService.allNews(), HttpStatus.OK);
+  }
 
-    @GetMapping("/source/{sourceId}")
-    public ResponseEntity<List<News>> newsBySource_id(@PathVariable String sourceId){
-        return new ResponseEntity<>(newsService.newsBySource_id(sourceId), HttpStatus.OK);
-    }
+  @GetMapping("/source/{sourceId}")
+  public ResponseEntity<List<News>>
+  newsBySource_id(@PathVariable String sourceId) {
+    return new ResponseEntity<>(newsService.newsBySource_id(sourceId),
+                                HttpStatus.OK);
+  }
 
-    @GetMapping("/category/{category}")
-    public ResponseEntity<List<News>> getNewsByCategoryOrderByPubDate(@PathVariable String category){
-        return new ResponseEntity<>(newsService.newsByCategoryOrderByPubDate(category), HttpStatus.OK);
-    }
-
-
+  @GetMapping("/category/{category}")
+  public ResponseEntity<List<News>>
+  getNewsByCategoryOrderByPubDate(@PathVariable String category) {
+    return new ResponseEntity<>(
+        newsService.newsByCategoryOrderByPubDate(category), HttpStatus.OK);
+  }
 }
