c69fca2a597376a7863d1615752feb59f88fa605
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2023-05-24 14:30:07 -0700

    Added notfications and commented out strictmode

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <ChakraProvider>
       <BrowserRouter>
         <Routes>
@@ -15,5 +15,5 @@ root.render(
         </Routes>
       </BrowserRouter>
     </ChakraProvider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
