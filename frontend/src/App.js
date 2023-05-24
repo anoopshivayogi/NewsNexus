beb4fd89a316a8c570fef9780e4c133b005555df
Restyled.io <commits@restyled.io>
2023-05-24 00:37:28 +0000

    Restyled by prettier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,16 @@
-import './App.css';
+import "./App.css";
 
-import {ColorModeScript} from '@chakra-ui/react'
-import {useEffect, useState} from 'react';
+import { ColorModeScript } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
-import api from './api/axiosConfig';
-import DataTabs from './components/DataTabs';
-import Nav from './components/Nav';
+import api from "./api/axiosConfig";
+import DataTabs from "./components/DataTabs";
+import Nav from "./components/Nav";
 
 // Login component
-function Login({handleLogin}) {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+function Login({ handleLogin }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -25,11 +25,10 @@ function Login({handleLogin}) {
   return (
     <form onSubmit={handleSubmit}>
       <input
-  type = "text"
-  placeholder = "Username"
+        type="text"
+        placeholder="Username"
         value={username}
-        onChange={
-    (e) => setUsername(e.target.value)}
+        onChange={(e) => setUsername(e.target.value)}
       />
       <input
         type="password"
@@ -43,55 +42,55 @@ function Login({handleLogin}) {
 }
 
 function groupObjectsByCategory(objects) {
-        const result = {};
+  const result = {};
 
-        objects.forEach((obj) => {
-          const {category, ...restData} = obj;
+  objects.forEach((obj) => {
+    const { category, ...restData } = obj;
 
-          if (!result[category]) {
-            result[category] = [];
-          }
+    if (!result[category]) {
+      result[category] = [];
+    }
 
-          result[category].push(restData);
-        });
+    result[category].push(restData);
+  });
 
-        return result;
+  return result;
 }
 
 function App() {
-        const [loggedIn, setLoggedIn] = useState(false);
-        const [news, setNews] = useState([]);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [news, setNews] = useState([]);
 
-        const handleLogin = () => { setLoggedIn(true); };
+  const handleLogin = () => {
+    setLoggedIn(true);
+  };
 
-        const getNews =
-            async () => {
-          try {
-            const response = await api.get("/api/v1/news");
-            console.log(response.data);
-            const newsData = groupObjectsByCategory(response.data);
-            setNews(newsData);
-            console.log(newsData);
-          } catch (err) {
-            console.log(err);
-          }
-        }
+  const getNews = async () => {
+    try {
+      const response = await api.get("/api/v1/news");
+      console.log(response.data);
+      const newsData = groupObjectsByCategory(response.data);
+      setNews(newsData);
+      console.log(newsData);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-        useEffect(() => { getNews(); }, []);
+  useEffect(() => {
+    getNews();
+  }, []);
 
-        if (!loggedIn) {
-        return <Login handleLogin =
-                { handleLogin
-                } />;
+  if (!loggedIn) {
+    return <Login handleLogin={handleLogin} />;
   }
 
   return (
     <div className="App">
       <ColorModeScript initialColorMode={"dark"} />
-               <Nav setData =
-                { setNews } />
-      <DataTabs data={news} /><
-               /div>
+      <Nav setData={setNews} />
+      <DataTabs data={news} />
+    </div>
   );
 }
 
