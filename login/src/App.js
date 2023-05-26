1d565ec58dd021e501aedf2c3263211215721b48
Restyled.io <commits@restyled.io>
2023-05-26 02:13:47 +0000

    Restyled by prettier

--- login/src/App.js
+++ login/src/App.js
@@ -1,46 +1,47 @@
-import './App.css';
+import "./App.css";
 
-import {GoogleLogin, useGoogleLogin} from '@react-oauth/google';
-import axios from "axios"
+import { GoogleLogin, useGoogleLogin } from "@react-oauth/google";
+import axios from "axios";
 import jwt_decode from "jwt-decode";
 
-import logo from './logo.svg';
+import logo from "./logo.svg";
 
 function App() {
-
   const login = useGoogleLogin({
-    onSuccess : async respose => {
+    onSuccess: async (respose) => {
       try {
         const res = await axios.get(
-            "https://www.googleapis.com/oauth2/v3/userinfo",
-            {headers : {"Authorization" : `Bearer ${respose.access_token}`}})
+          "https://www.googleapis.com/oauth2/v3/userinfo",
+          { headers: { Authorization: `Bearer ${respose.access_token}` } }
+        );
 
-        console.log(res.data)
+        console.log(res.data);
       } catch (err) {
-        console.log(err)
+        console.log(err);
       }
-    }
+    },
   });
 
-    return (
-        <div className="App">
-            <header className="App-header">
-                <button onClick={login}>
-                    <i class="fa-brands fa-google"></i>
-                    Continue with google
-                </button>
-                <GoogleLogin
-    onSuccess = {
-      credentialResponse => {
-        console.log(credentialResponse.credential);
-        var decoded = jwt_decode(credentialResponse.credential);
-        console.log(decoded)
-      }
-    } onError = {
-      () => { console.log('Login Failed'); }
-    } />
-            </header > < /div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={login}>
+          <i class="fa-brands fa-google"></i>
+          Continue with google
+        </button>
+        <GoogleLogin
+          onSuccess={(credentialResponse) => {
+            console.log(credentialResponse.credential);
+            var decoded = jwt_decode(credentialResponse.credential);
+            console.log(decoded);
+          }}
+          onError={() => {
+            console.log("Login Failed");
+          }}
+        />
+      </header>{" "}
+    </div>
+  );
 }
 
 export default App;
