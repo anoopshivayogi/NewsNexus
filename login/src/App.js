b221b9b4d7b0a31fcdbee521dc1816123d5f2ade
Restyled.io <commits@restyled.io>
2023-05-26 02:13:43 +0000

    Restyled by clang-format

--- login/src/App.js
+++ login/src/App.js
@@ -1,29 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-import {GoogleLogin} from '@react-oauth/google';
-import jwt_decode from "jwt-decode";
-import {useGoogleLogin} from '@react-oauth/google';
+
+import {GoogleLogin, useGoogleLogin} from '@react-oauth/google';
 import axios from "axios"
+import jwt_decode from "jwt-decode";
+
+import logo from './logo.svg';
 
 function App() {
 
-    const login = useGoogleLogin({
-        onSuccess: async respose => {
-            try {
-                const res = await axios.get("https://www.googleapis.com/oauth2/v3/userinfo", {
-                    headers: {
-                        "Authorization": `Bearer ${respose.access_token}`
-                    }
-                })
+  const login = useGoogleLogin({
+    onSuccess : async respose => {
+      try {
+        const res = await axios.get(
+            "https://www.googleapis.com/oauth2/v3/userinfo",
+            {headers : {"Authorization" : `Bearer ${respose.access_token}`}})
 
-                console.log(res.data)
-            } catch (err) {
-                console.log(err)
-
-            }
-
-        }
-    });
+        console.log(res.data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  });
 
     return (
         <div className="App">
@@ -33,16 +30,16 @@ function App() {
                     Continue with google
                 </button>
                 <GoogleLogin
-                    onSuccess={credentialResponse => {
-                    console.log(credentialResponse.credential);
-                    var decoded = jwt_decode(credentialResponse.credential);
-                    console.log(decoded)
-                }}
-                    onError={() => {
-                    console.log('Login Failed');
-                }}/>
-            </header>
-        </div>
+    onSuccess = {
+      credentialResponse => {
+        console.log(credentialResponse.credential);
+        var decoded = jwt_decode(credentialResponse.credential);
+        console.log(decoded)
+      }
+    } onError = {
+      () => { console.log('Login Failed'); }
+    } />
+            </header > < /div>
     );
 }
 
--- login/src/index.js
+++ login/src/index.js
@@ -1,17 +1,21 @@
+import './index.css';
+
+import {GoogleOAuthProvider} from '@react-oauth/google';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {GoogleOAuthProvider} from '@react-oauth/google';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-//client-id: 167380039775-cqfkpfg5vui029p0l6693t6fa106r9e0.apps.googleusercontent.com
+// client-id:
+// 167380039775-cqfkpfg5vui029p0l6693t6fa106r9e0.apps.googleusercontent.com
 
 root.render(
     <React.StrictMode>
-        <GoogleOAuthProvider clientId="167380039775-cqfkpfg5vui029p0l6693t6fa106r9e0.apps.googleusercontent.com">
-            <App/></GoogleOAuthProvider>;
-    </React.StrictMode>
-);
+    <GoogleOAuthProvider clientId =
+         "167380039775-cqfkpfg5vui029p0l6693t6fa106r9e0.apps.googleusercontent.com">
+    <App /></GoogleOAuthProvider>;
+    </React.StrictMode>);
 
 reportWebVitals();
