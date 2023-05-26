b33348557323c1be66e2659ce180c78b2ed8038b
Restyled.io <commits@restyled.io>
2023-05-26 02:22:32 +0000

    Restyled by prettier

--- frontend/src/components/DataTabs.js
+++ frontend/src/components/DataTabs.js
@@ -14,55 +14,56 @@ import {
   TabPanel,
   TabPanels,
   Tabs,
-  Text
-} from '@chakra-ui/react'
+  Text,
+} from "@chakra-ui/react";
 
-function DataTabs({data}) {
-      return (
-        <Tabs marginTop="25px" marginLeft="20px" size="lg">
-          <TabList>
-            {Object.keys(data)?.map((tab, index) => (
-              <Tab key={index}>{tab}</Tab>
-            ))}
-          </TabList>
-          <TabPanels>
-            {Object.keys(data)?.map((tab, index) => (
-                <TabPanel key={index} p={4}>
-                  <Flex flexWrap="wrap" justifyContent="flex-start">
-                    {data[tab]?.map((item, innerIndex) => (
-                    <Link href={item.link} isExternal>
-                      <Card key={innerIndex} maxW='md' mb={4} margin={3}>
-                        <CardBody>
-                          <Image
-                            src={item.image_url}
-                            alt={item.title}
-                            borderRadius='lg'
-                          />
-                          <Stack mt='6' spacing='3'>
-                            <Heading size='md'>{item.title}</Heading>
-                            <Box height="100px" overflow="hidden">
-                            <Text>{item.description}</Text>
-                            </Box>
-                          </Stack>
-                        </CardBody>
-                        <Divider />
-                        <CardFooter>
-                          <em style={{
-      fontStyle: "italic" }}>
-                            source: {item.source_id}
-                          </em>
-                        </CardFooter>
-                      </Card>
-                    </Link>
-                    ))}
-                  </Flex>
-                </TabPanel>
+function DataTabs({ data }) {
+  return (
+    <Tabs marginTop="25px" marginLeft="20px" size="lg">
+      <TabList>
+        {Object.keys(data)?.map((tab, index) => (
+          <Tab key={index}>{tab}</Tab>
+        ))}
+      </TabList>
+      <TabPanels>
+        {Object.keys(data)?.map((tab, index) => (
+          <TabPanel key={index} p={4}>
+            <Flex flexWrap="wrap" justifyContent="flex-start">
+              {data[tab]?.map((item, innerIndex) => (
+                <Link href={item.link} isExternal>
+                  <Card key={innerIndex} maxW="md" mb={4} margin={3}>
+                    <CardBody>
+                      <Image
+                        src={item.image_url}
+                        alt={item.title}
+                        borderRadius="lg"
+                      />
+                      <Stack mt="6" spacing="3">
+                        <Heading size="md">{item.title}</Heading>
+                        <Box height="100px" overflow="hidden">
+                          <Text>{item.description}</Text>
+                        </Box>
+                      </Stack>
+                    </CardBody>
+                    <Divider />
+                    <CardFooter>
+                      <em
+                        style={{
+                          fontStyle: "italic",
+                        }}
+                      >
+                        source: {item.source_id}
+                      </em>
+                    </CardFooter>
+                  </Card>
+                </Link>
+              ))}
+            </Flex>
+          </TabPanel>
+        ))}
+      </TabPanels>
+    </Tabs>
+  );
+}
 
-            ))}
-          </TabPanels>
-        </Tabs>
-      );
-    }
-    
-  
 export default DataTabs;
