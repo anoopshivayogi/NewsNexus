c74d2d88ed0a739df210561d77425a540b4bf408
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2023-09-11 01:35:39 +0530

    axios change for heroku deployment

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 // Avoid CORS error - Cross origin error
 export default axios.create({
-    baseURL: 'http://localhost:8080',
+    // baseURL: 'http://localhost:8080',
+    baseURL: 'https://newsnexus-backend.anoopshivayogi.com',
     headers: {"ngrok-skip-browser-warning": "true", "Access-Control-Allow-Origin": "*"}
 });
